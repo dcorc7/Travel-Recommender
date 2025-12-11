088c31b3c2c1f24f62efe353a55055f1900c64db
Adam Stein <ahs131@georgetown.edu>
2025-12-11 17:01:28 -0500

    Made changes to CI for pytest

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,12 +3,12 @@ import pytest
 from backend.src.api.bm25_utils import search_bm25
 from backend.src.api.modern_bert_utils import search_modernbert
 
-'''
+
 @pytest.fixture
 def queries():
     with open("backend/data/queries.json", "r") as f:
         return json.load(f)["queries"]
-'''
+
 
 @pytest.fixture
 def run_bm25():
