1a39f71a325fe362ae0044e9c7e06d84bb77ce65
dcorc7 <dc1524@georgetown.edu>
2025-12-13 12:39:13 -0500

    Created website skeleton

---
+++ website/GUdomains.sh
@@ -0,0 +1,26 @@
+
+
+echo "Cleaning up existing build..."
+rm -rf Travel-Recommender
+
+# build website
+echo "Building the website..."
+quarto render
+
+# Set Correct File Permissions
+echo "Setting correct file permissions..."
+
+find Travel-Recommender -type f -exec chmod 644 {} \;
+
+find Travel-Recommender -type d -exec chmod 755 {} \;
+
+# push to the Website
+read -p "Do you want to push the website to your GU domains folder? (y/n): " answer
+
+if [[ "$answer" == "y" || "$answer" == "Y" ]]; then
+    echo "Pushing the website to the remote server..."
+    scp -r Travel-Recommender corcoran@corcoran.georgetown.domains:/home/corcoran/public_html/
+    echo "Website successfully pushed!"
+else
+    echo "Skipping deployment."
+fi
